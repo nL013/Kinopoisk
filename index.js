7ef058d35184923d569f782b2bcde6f25e1498cc
nL013 <dinamit10d@gmail.com>
2024-10-26 19:33:21 +0500

    commit

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const API_KEY = "b21f1939"
+const API_KEY = "b21f1939";
 
 async function fetchData(title) {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&t="${title}"`)
-    const data = await response.json()
-    return data
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=${API_KEY}&t="${title}"`
+  );
+  const data = await response.json();
+  return data;
 }
 
-const searchInputElement = document.querySelector('#movie-search-input')
-const searchButtonElement= document.querySelector('#movie-search-button')
+const searchInputElement = document.querySelector("#movie-search-input");
+const searchButtonElement = document.querySelector("#movie-search-button");
 
-let movieTitleValue = ''
+let movieTitleValue = "";
 
-searchButtonElement.addEventListener('click', async () => {
-    movieTitleValue = searchInputElement.value
-    const movie = await fetchData(movieTitleValue)
-    const cardElementTemplate = `<div class="card" style="width: 18rem">
+searchButtonElement.addEventListener("click", async () => {
+  movieTitleValue = searchInputElement.value;
+  const movie = await fetchData(movieTitleValue);
+  const cardElementTemplate = `<div class="card" style="width: 18rem">
         <img
         src="${movie.Poster}"
         class="card-img-top"
@@ -32,16 +34,33 @@ searchButtonElement.addEventListener('click', async () => {
                 Подробнее
             </a>
         </div>
-    </div>`
-    
-    const searchResultsContainer = document.querySelector('.search-results')
+    </div>`;
 
-    console.log(searchResultsContainer.children)
-    
-    searchResultsContainer.insertAdjacentHTML('beforeend', cardElementTemplate)
-})
+  const searchResultsContainer = document.querySelector(".search-results");
 
+  console.log(searchResultsContainer.children);
 
+  searchResultsContainer.insertAdjacentHTML("beforeend", cardElementTemplate);
+});
 
+//22.10.2024
 
+console.log(localStorage.getItem("dateNow"));
 
+const phoneNumbers = ["998909632050", "998901234567"];
+localStorage.setItem("phoneNumbers", phoneNumbers);
+
+const myData = {
+  age: 25,
+  sex: "male",
+  pets: ["dogName"],
+};
+
+localStorage.setItem("MyData", JSON.stringify(myData));
+
+let myDataJson = localStorage.getItem("myData");
+let MyParsedData = JSON.parse(myDataJson);
+myData.pets[0];
+
+// localStorage.setItem(length)
+print.localStorage(length)
