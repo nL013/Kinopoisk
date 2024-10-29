ba651e6089ba4943f7ac29dbb71769967405486b
nL013 <dinamit10d@gmail.com>
2024-10-29 11:48:14 +0500

    commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const API_KEY = "b21f1939";
 
 async function fetchData(title) {
+  const spiner = document.querySelector("#spiner");
+  spiner.style.display = "block";
   const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&t="${title}"`
+    `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&t="${title}"`
   );
   const data = await response.json();
+  spiner.style.display = "none";
   return data;
 }
 
@@ -12,55 +15,107 @@ const searchInputElement = document.querySelector("#movie-search-input");
 const searchButtonElement = document.querySelector("#movie-search-button");
 
 let movieTitleValue = "";
+let movie = null;
 
 searchButtonElement.addEventListener("click", async () => {
-  movieTitleValue = searchInputElement.value;
-  const movie = await fetchData(movieTitleValue);
-  const cardElementTemplate = `<div class="card" style="width: 18rem">
-        <img
-        src="${movie.Poster}"
-        class="card-img-top"
-        alt="${movie.Title} movie poster"
-        />
-        <div class="card-body">
-            <h5 class="card-title">${movie.Title}</h5>
-            <p class="card-text">${movie.Plot}</p>
-            <a
-                href="#"
-                class="btn btn-primary"
-                data-bs-toggle="modal"
-                data-bs-target="#exampleModal"
-                >
-                Подробнее
-            </a>
-        </div>
-    </div>`;
-
   const searchResultsContainer = document.querySelector(".search-results");
 
-  console.log(searchResultsContainer.children);
+  movieTitleValue = searchInputElement.value;
 
+  if (
+    movie &&
+    movie.Title.toLowerCase().includes(movieTitleValue.toLocaleLowerCase())
+  )
+    return;
+  searchResultsContainer.innerHTML = "";
+
+  movie = await fetchData(movieTitleValue);
+
+  if (movie.Response === "False") {
+    alertMessage("movie not found", "error");
+    return;
+  }
+
+  const cardElementTemplate = `
+  <div class="card" style="width: 18rem">
+      <img
+      src="${movie.Poster}"
+      class="card-img-top"
+      alt="${movie.Title} movie poster"
+      />
+      <div class="card-body">
+          <h5 class="card-title">${movie.Title}</h5>
+          <p class="card-text">${movie.Plot}</p>
+          <a
+              href="#"
+              class="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              >
+              Подробнее
+          </a>
+
+            <a
+              href="#"
+              class="btn btn-primary add-fav-btn" style="margin-top: 10px;"
+              >
+            Добавить в избранное
+          </a>
+      </div>
+  </div>`;
   searchResultsContainer.insertAdjacentHTML("beforeend", cardElementTemplate);
+  alertMessage("success", "success");
+
+  searchResultsContainer.innerHTML = "";
+  searchResultsContainer.insertAdjacentHTML("beforeend", cardElementTemplate);
+  let addedMovie = movie;
+
+  const addFavButton = document.querySelector(".add-fav-btn");
+  addFavButton.addEventListener("click", () => {
+    if (localStorage.getItem("favMovies") === null) {
+      const favMoviesList = [];
+      favMoviesList.push(movie);
+      localStorage.setItem("favMovies", JSON.stringify(favMoviesList));
+      return;
+    }
+
+    const favMoviesList = JSON.parse(localStorage.getItem("favMovies"));
+    favMoviesList.push(movie);
+    localStorage.setItem("favMovies", JSON.stringify(favMoviesList));
+  });
 });
 
-//22.10.2024
+const ModalELem = document.getElementById("exampleModal");
 
-console.log(localStorage.getItem("dateNow"));
+ModalELem.addEventListener("show.bs.modal", (event) => {
+  console.log("hello");
+  document.getElementById("exampleModalLabel").textContent = movie.Title;
+  document.querySelector("#cardTitle").textContent = movie.Title;
+  document.getElementById("modal-body-image").src = movie.Poster;
 
-const phoneNumbers = ["998909632050", "998901234567"];
-localStorage.setItem("phoneNumbers", phoneNumbers);
+  document.getElementById("cardActors").textContent = movie.Actors;
+  document.getElementById("cardPlot").textContent = movie.Plot;
+  document.getElementById("cardYear").textContent = movie.Year;
+  document.getElementById("cardRating").textContent = movie.Rating;
+  document.getElementById("cardReleased").textContent = movie.Released;
+  document.getElementById("cardType").textContent = movie.Type;
+});
 
-const myData = {
-  age: 25,
-  sex: "male",
-  pets: ["dogName"],
-};
+const modalBoxesElement = document.querySelector("#modal-boxes");
 
-localStorage.setItem("MyData", JSON.stringify(myData));
+function alertMessage(message, type) {
+  const toastBody = document.querySelector("#toast-body");
+  let toast = document.getElementById("toast");
 
-let myDataJson = localStorage.getItem("myData");
-let MyParsedData = JSON.parse(myDataJson);
-myData.pets[0];
+  toastBody.textContent = message;
 
-// localStorage.setItem(length)
-print.localStorage(length)
+  if (type === "success") {
+    toast.style.border = "1px solid green";
+    toastBody.style.color = "green";
+  } else if (type === "error") {
+    toast.style.border = "1px solid red";
+    toastBody.style.color = "red";
+  }
+
+  $(".toast").toast("show");
+}
